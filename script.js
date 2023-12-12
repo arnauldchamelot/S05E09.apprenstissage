f7f03288338fa63600f234f1e28587f893d183be
arnauldchamelot <arnauld.chamelot@oclock.school>
2023-12-12 11:59:59 +0100

    petit taf

---
+++ script.js
@@ -0,0 +1,16 @@
+function addition(a, b) {
+    const result = a + b;
+    return result;
+}
+
+const resultat = addition(10, 20);
+
+console.log(resultat);
+
+function decrit(prenom, age, habite){
+    const message = `Bonjour ${prenom}, tu as ${age} ans, et tu habites ${habite}`;
+    return message
+}
+const newmessage = decrit("Hadi",30,"Lyon");
+console.log(newmessage);
+
